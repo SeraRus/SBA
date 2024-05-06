23ade02c5543fe388becea1b6f59888c4c8ee396
SeraRus <51085940+SeraRus@users.noreply.github.com>
2024-05-06 23:17:53 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="zh_hk">
+<?php
+header('location: home.html');
+exit();
+?>
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
